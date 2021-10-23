aaecd313f8f5f7b6976e94dbb97292bc02f77759
Utkarsh <Utkarshseth2019@gmail.com>
2021-10-23 16:39:45 +0530

    Some styling and sorting functions

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
     const { monsters, searchField } = this.state;
@@ -27,9 +30,10 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Mosters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMosters} />
       </div>
